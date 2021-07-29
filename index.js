e86a1de412a6e8c231378f9ff8bc0b817492cda6
WillyChristian <crisaort@gmail.com>
2021-07-28 22:48:42 -0300

    update index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const source = require("./public/lib/source");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
